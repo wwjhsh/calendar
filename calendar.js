d02e61c5499ff46a2ce78878a8a367ea82a3d176
wwjhsh <wwjelo@163.com>
2017-07-11 18:17:12 +0800

    可以查看上下月

--- calendar.js
+++ calendar.js
@@ -12,8 +12,8 @@ $.fn.calendar=function(options){
 		if ( $("#calendar").length > 0 ) { $("#calendar").remove(); }
 		var _this = {
 			targetId:               "",
-			begindate:           	new Date("2008/2/02"),
-			enddate: 				new Date("2008/9/09"),
+			begindate:           	new Date("2008/02/02"),
+			enddate: 				new Date("2008/09/09"),
 			choosedyear: 			2008,
 			choosedmonth: 			9,
 			selectback : 			function(){ }, //选择日期的事件回调
@@ -38,11 +38,9 @@ $.fn.calendar.setContainer=function($this,_this){
 	var $calendarDiv=$("<div id="+_this.targetId+"></div>");
 	var _y=$this.offset().top+30;
 	var _x=$this.offset().left;
-	console.log(_y);
-	console.log(_x);
-	var $form=$('<form class="layui-form" action=""></form>').appendTo($calendarDiv);
+	var $form=$('<form></form>').appendTo($calendarDiv);
 	$("<div class='time-choose'></div>").appendTo($form);
-	var $table=$("<table class='layui-table calendar-day'></table>").appendTo($form);
+	var $table=$("<table class='calendar-table calendar-day'></table>").appendTo($form);
 	var $thead=$("<thead></thead>").appendTo($table);
 	var $tbody=$("<tbody></tbody>").appendTo($table);
 	var $tr=$("<tr></tr>").appendTo($thead);
@@ -71,12 +69,6 @@ $.fn.calendar.setMonth=function($this){
 			$this._endmonth=12;
 		}
 	}
-	console.log($this._beginyear);
-	console.log($this._endyear);
-	console.log($this._beginmonth);
-	console.log($this._endmonth);
-	console.log($this._choosedyear);
-	console.log($this._choosedmonth);
 };
 //组装选择器
 $.fn.calendar.setPicker=function($this,_this){
@@ -122,17 +114,68 @@ $.fn.calendar.setPicker=function($this,_this){
 	$select_item2.appendTo($in_inline2);
 	$out_inline2.append($lable2).append($in_inline2);
 	$("#"+_this.targetId+" .time-choose").append($out_inline2);
+	//组装上下月按键
+	$("<div></div>").addClass("calendar-last").text("上月").prependTo($("#"+_this.targetId+" .time-choose"));
+	$("<div></div>").addClass("calendar-next").text("下月").prependTo($("#"+_this.targetId+" .time-choose"));
 	// 组装好的选择器添加方法
 	$("#"+_this.targetId+" select.calendar-year").change(function(){
 		console.log($(this).val());
 		$this._choosedyear=$(this).val();
-		$this._choosedmonth=1;
+		$.fn.calendar.setMonth($this);
+		$this._choosedmonth=$this._beginmonth;
 		$.fn.calendar.setPicker($this,_this);
 		$.fn.calendar.setDateData($this,_this);
 	});
 	$("#"+_this.targetId+" select.calendar-month").change(function(){
 		$this._choosedmonth=$(this).val();
 		$.fn.calendar.setDateData($this,_this);
+		$.fn.calendar.setMonthNear($this,_this);
+	});
+	$.fn.calendar.setMonthNear($this,_this);
+}
+$.fn.calendar.setMonthNear=function($this,_this){
+	var _date01=new Date();
+	_date01.setFullYear($this._choosedyear,$this._choosedmonth-2,1);//得到上月份1号的日期
+	console.log(_date01);
+	if (($.fn.calendar.isInRange($this,Date.UTC($this._choosedyear,$this._choosedmonth-2,$.fn.calendar.getMonthDateAmount(_date01))))
+		||($.fn.calendar.isInRange($this,Date.UTC($this._choosedyear-1,11,31)))) {
+		$("#"+_this.targetId+" .calendar-last").css("visibility","visible");
+	}
+	else{
+		$("#"+_this.targetId+" .calendar-last").css("visibility","hidden");
+	}
+	if (($.fn.calendar.isInRange($this,Date.UTC($this._choosedyear,$this._choosedmonth,1)))
+		||($.fn.calendar.isInRange($this,Date.UTC($this._choosedyear+1,0,1)))) {
+		$("#"+_this.targetId+" .calendar-next").css("visibility","visible");
+	}
+	else{
+		$("#"+_this.targetId+" .calendar-next").css("visibility","hidden");
+	}
+	$("#"+_this.targetId+" .calendar-last").on("click",function(){
+		if ($this._choosedmonth>1) {
+			$this._choosedmonth--;
+			$.fn.calendar.setPicker($this,_this);
+			$.fn.calendar.setDateData($this,_this);
+		}else{
+			$this._choosedmonth=12;
+			$this._choosedyear--;
+			$.fn.calendar.setMonth($this);
+			$.fn.calendar.setPicker($this,_this);
+			$.fn.calendar.setDateData($this,_this);
+		}
+	});
+	$("#"+_this.targetId+" .calendar-next").on("click",function(){
+		if ($this._choosedmonth<12) {
+			$this._choosedmonth++;
+			$.fn.calendar.setPicker($this,_this);
+			$.fn.calendar.setDateData($this,_this);
+		}else{
+			$this._choosedmonth=1;
+			$this._choosedyear++;
+			$.fn.calendar.setMonth($this);
+			$.fn.calendar.setPicker($this,_this);
+			$.fn.calendar.setDateData($this,_this);
+		}
 	});
 }
 //组装日历主体部分
@@ -149,26 +192,25 @@ $.fn.calendar.setDateData=function($this,_this){
 	_date_last.setFullYear(_choosedyear,_choosedmonth-1,_dayAmount);//得到所选择月份最后一天的日期
 	var _date_last_day=_date_last.getDay();
 	var num=1;
-
 	setDate:for (var i = 0;; i++) 
 	{
 		var $trItem=$("<tr></tr>");
 		if (i==0) {
 			if (_date_day!=0) {
 				for (var j = 0; j <_date_day; j++) {
-					$("<th></th>").appendTo($trItem);
+					$("<td></td>").addClass("forbid").appendTo($trItem);
 				}
 			}
 			for (var j = _date_day; j < 7; j++,num++) {
-				$("<th></th>").text(num).appendTo($trItem);
+				$("<td></td>").text(num).appendTo($trItem);
 			}
 		}else{
 			for (var j = 0; j < 7; j++,num++) {
-				$("<th></th>").text(num).appendTo($trItem);
+				$("<td></td>").text(num).appendTo($trItem);
 				if (num==_dayAmount) {
 					if (_date_last_day<6) {
 						for (var j = _date_last_day+1; j < 7; j++,num++) {
-							$("<th></th>").appendTo($trItem);
+							$("<td></td>").addClass("forbid").appendTo($trItem);
 						}
 					}
 					$trItem.appendTo($datePlace);
@@ -179,7 +221,6 @@ $.fn.calendar.setDateData=function($this,_this){
 		$trItem.appendTo($datePlace);
 	}
 }
-
 //得到某一天月份天数
 $.fn.calendar.getMonthDateAmount=function(date){
 	var month=date.getMonth()+1;
@@ -209,5 +250,18 @@ $.fn.calendar.getMonthDateAmount=function(date){
 		}else {
 			return 28;
 		}
+	}
+}
+//判断一个时间是否在某个时间范围内
+$.fn.calendar.isInRange=function($this,date){
+	var _begindate=Date.UTC($this._begindate.getFullYear(),$this._begindate.getMonth(),$this._begindate.getDate()),
+		_enddate=Date.UTC($this._enddate.getFullYear(),$this._enddate.getMonth(),$this._enddate.getDate());
+	// date=Date.UTC(date.getFullYear(),date.getMonth()+1,date.getDate());
+	if ((date<=_enddate)&&(date>=_begindate)) {
+		console.log(_begindate);
+		console.log(Date.UTC("2012/2/02"));
+		return true;
+	}else {
+		return false;
 	}
 }
